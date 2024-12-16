7f93c9b1b0a9557a66b95b199deebb118bfea495
alexander <alex@cedarcedar.com>
2024-12-16 13:48:44 -0500

    Update register.js

--- api/register.js
+++ api/register.js
@@ -4,7 +4,8 @@ import { createToken } from "../util.js";
 
 export default async function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  // allow post and options
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   // for options and cors -> do your thing
